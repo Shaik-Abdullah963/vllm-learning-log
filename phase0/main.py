39c87364139309af89f0eba949938c24ee49f3f3
Abdullah Shaik <abdullah.shaik.php@gmail.com>
2026-01-01 21:20:06 -0600

    phase0: add end-to-end latency (E2EL) measurement

--- phase0/main.py
+++ phase0/main.py
@@ -20,10 +20,15 @@ def main():
     first_token_time = None
     generated_text = ""
     for new_text in streamer:
+        now = time.perf_counter()
         if first_token_time is None:
             first_token_time = time.perf_counter()
+            # Time to first token
             print(f"\n TTFT: {first_token_time - t0:.4f}s\n")
+        last_token_time = now
         generated_text += new_text
+    # Total Latency or End to End Latency(E2EL)
+    print(f"\n E2EL: {last_token_time - t0:.4f}s\n")
     print(generated_text)
     thread.join()
 
